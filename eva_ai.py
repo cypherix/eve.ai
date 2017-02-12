c0a3f1e82cf69795cd44c95b1189ae4dedc9d1e1
Rohit Anil <rohitanil7121993@gmail.com>
2017-02-13 01:34:14 +0530

    Score generation for resumes have been implemented

--- eva_ai.py
+++ eva_ai.py
@@ -9,13 +9,17 @@ import string
 
 frameworks=[("php","laravel"),("javascript","nodejs"),("python","django"),("python","data analytics"),("php","codeignite")]
 skill_dict=["php","javascript","html","c#","asp.net","java","laravel","nodejs","django","python","ml","c","c++","mysql","mongodb","postgresql","ajax"]
+college_dict=[('Sree Chitra Thirunal College Of Engineering Trivandrum',5.0),('University Of California Irwine',5.0),('Harvard University',5.0)]
+company_dict=[('Google ',5.0),('Microsoft ',5.0),('TCS ',3.0),('Freshdesk ',4.0)]
 start=time.time()
 college=""
-l=""
+company=""
 c=""
+college_score=0.0
+company_score=0.0
 
 conclusion2=""
-final_list=[] #final list of colleges
+
 
 
 #Location of file
@@ -26,7 +30,7 @@ with open('/Users/continuumlabs/Desktop/stanford/resume.txt', 'r') as f:
     if re.search(r'(Skills|SKILLS|Technologies|TECHNOLOGIES)((:|\s|-|:-)*)((\r\n|\r|\n)*)((.*?|\r\n|\r|\n)*)([A-z])(:|-|(\n)+]{1,})', data):
         c2=re.search(r'(Skills|SKILLS|Technologies|TECHNOLOGIES)((:|\s|-|:-)*)((\r\n|\r|\n)*)((.*?|\r\n|\r|\n)*)([A-z])(:|-|(\n)+]{1,})', data).group(0)
         c2=c2.lower()
-        print c
+        #print c
         stop_words=set(stopwords.words('english'))
         stop_words=set(string.punctuation)
         resume1=[i for i in wordpunct_tokenize(c2) if i  not in stop_words]
@@ -36,17 +40,17 @@ with open('/Users/continuumlabs/Desktop/stanford/resume.txt', 'r') as f:
         print skill_list
         skill_match=[]
 
-        jd=["javascript","html","autocad"]#jd
-        print "\nJD skills "
-        print jd
+        jd=["javascript","html","ml","php"]#jd
+        #print "\nJD skills "
+        #print jd
         ##append frameworks to new jd list
         jd1=jd
         for i in range(0,len(jd)):
          for (x,y) in frameworks:
           if(x==jd[i]):
            jd1.append(y)
-        print "JD skills after processing"
-        print jd1
+        #print "JD skills after processing"
+        #print jd1
     ##search for skills in the new jd list but use old jd list for score calculation
         for i in range(0,len(jd1)):
             for j in range(0,len(skill_list)):
@@ -82,40 +86,67 @@ with open('/Users/continuumlabs/Desktop/stanford/resume.txt', 'r') as f:
         conclusion2=re.search(r'(Work Experience|WORK EXPERIENCE)((:|\s|-|:-)*)((\r\n|\r|\n)*)((.*?|\r\n|\r|\n)*)([A-z])(:|-|(\n)+]{1,})', data).group(0)
         
     college+=str(conclusion1)
-    college+=str(conclusion2)
-    c =college.split(' ',1)[1]
-    if c[-1]!=".":
-        c+="."  #to determine the end of extracted paragraph(if there is no fullstop)
+    company+=str(conclusion2)
+    c1 =college.split(' ',1)[1]
+    if c1[-1]!=".":
+        c1+="."  #to determine the end of extracted paragraph(if there is no fullstop)
 
+    c2 =company.split(' ',1)[1]
+    if c2[-1]!=".":
+        c2+="."  #to determine the end of extracted paragraph(if there is no fullstop)
+
+ 
     f.seek(0,0) #point to beginning of file
     firstNlines=f.readlines()[0:5]#read first 5 lines
     firstNlines= list(map(lambda s: s.strip(), firstNlines))#strip newline characters 
     data_sect2=re.findall(r'[\w\.-]+@[\w\.-]+',data)#email regex
     email=str(data_sect2)  
-# Change the path according to your system
-stanford_classifier = '/Users/continuumlabs/Desktop/stanford/stanford-ner-2016-10-31/classifiers/english.all.3class.distsim.crf.ser.gz'
-stanford_ner_path = '/Users/continuumlabs/Desktop/stanford/stanford-ner-2016-10-31/stanford-ner.jar'
 
-# Creating Tagger Object
-st = StanfordNERTagger(stanford_classifier, stanford_ner_path, encoding='utf-8')
-
-tokenized_text = word_tokenize(c)
-classified_text = st.tag(tokenized_text)
-for sublist in classified_text:
-    if sublist[1]=='ORGANIZATION':
-        l+=str(sublist[0])
-        l+=" "
-    elif sublist[1]=='LOCATION':
-        l+=str(sublist[0])
-                
-    elif sublist[0]=='.':
-        final_list.append(l)
+    def entity_recog(text):
         l=""
+        final_list=[] #final list of colleges
+        # Change the path according to your system
+        stanford_classifier = '/Users/continuumlabs/Desktop/stanford/stanford-ner-2016-10-31/classifiers/english.all.3class.distsim.crf.ser.gz'
+        stanford_ner_path = '/Users/continuumlabs/Desktop/stanford/stanford-ner-2016-10-31/stanford-ner.jar'
 
+        # Creating Tagger Object
+        st = StanfordNERTagger(stanford_classifier, stanford_ner_path, encoding='utf-8')
 
-final_list=list(filter(None,final_list))
+        tokenized_text = word_tokenize(text)
+        classified_text = st.tag(tokenized_text)
+        for sublist in classified_text:
+            if sublist[1]=='ORGANIZATION':
+                l+=str(sublist[0])
+                l+=" "
+            elif sublist[1]=='LOCATION':
+                l+=str(sublist[0])
+                        
+            elif sublist[0]=='.':
+                final_list.append(l)
+                l=""
+        final_list=list(filter(None,final_list))
+        return final_list
+
+    college_list=entity_recog(c1)
+    company_list=entity_recog(c2)
+    for i in range(0,len(college_list)):
+        for (x,y) in college_dict:
+            if(x==college_list[i]):
+                college_score=float(y)+college_score
+
+    college_score=(4*college_score)/len(college_list)
+    for i in range(0,len(company_list)):
+        for (x,y) in company_dict:
+            if(x==company_list[i]):
+                company_score=float(y)+company_score
+
+    company_score=(10*company_score)/len(company_list)
+ 
+ 
+    
 print "Extracted Details:"          
-print final_list
+print college_score
+print company_score
 #print (firstNlines)
 print email
 print "\nSCORE"
