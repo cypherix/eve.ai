77b6522920910aefd1838928be6f79f0ebbb733c
Rohit Anil <rohitanil7121993@gmail.com>
2017-02-14 01:02:47 +0530

    Updated dictionary values

--- dictionary.py
+++ dictionary.py
@@ -1,43 +1,20 @@
-import nltk.corpus
-from nltk.corpus import wordnet
-from nltk.tokenize.treebank import TreebankWordTokenizer
-from nltk.tokenize import word_tokenize
-from nltk.corpus import stopwords
-import string
+#COLLEGE LIST
 
+college_dict=[("Carnegie Mellon University",5.0),("Massachusetts Institute Of Technology",5.0),("Stanford University",5.0),("University Of California Berkeley",5.0),("University Of Illinois Urbana Champaign",4.6),("Cornell University",4.5),("University Of Washington",4.5),("Princeton University",4.4),("Georgia Institute Of Technology",4.3),("University Of Texas Austin",4.3),("California Institute Of Technolgy",4.2),
+      ("University Of Wisconsin-Madison",4.2),("University Of California Los Angeles",4.1),("University Of Michigan Ann Arbor",4.1),("Columbia University",4.0),("University of California-SanDiego",4.0),("University of Maryland—​College Park",4.0),
+      ("Harvard University",3.9),("University Of Pennsylvania",3.8),("Brown University",3.7),("Purdue University West Lafayette",3.7),("Rice University",3.7),("University Of Southern California",3.7),("Yale University",3.7),("Duke University",3.6),("University Of Massachusetts Amherst",3.6),("University Of North Carolina Chapel Hill",3.6),("Johns Hopkins University",3.5),("New York University",3.4),("Pennsylvania State University University Park",3.4),
+      ("University Of California Irwine",3.4),('Sree Chitra Thirunal College Of Engineering Trivandrum',3.0),("Amal Jyothi College Of Engineering Changanassery",2.0),("Lourdes Matha College Of Engineering",2.9)]
 
-university=("I have graduated from University of Illinois-Urbana-Champaign")
+#########
 
-#Dictionary containing universities' ratings
+#COMPANY LIST
 
-exDict={"Carnegie Mellon University":5.0,"Massachusetts Institute of Technology":5.0,"Stanford University":5.0,"University of California-Berkeley":5.0,"University of Illinois-Urbana-Champaign":4.6,"Cornell University":4.5,"University of Washington":4.5,"Princeton University":4.4,"Georgia Institute of Technology":4.3,"University of Texas-Austin":4.3,"California Institute of Technolgy":4.2,
-      "University of Wisconsin-Madison":4.2,"University of California-Los Angeles":4.1,"University of Michigan-Ann Arbor":4.1,"Columbia University":4.0,"University of California-SanDiego":4.0,"University of Maryland—​College Park":4.0,
-      "Harvard University":3.9,"University of Pennsylvania":3.8,"Brown University":3.7,"Purdue University-West Lafayette":3.7,"Rice University":3.7,"University of Southern California":3.7,"Yale University":3.7,"Duke University":3.6,"University of Massachusetts-Amherst":3.6,"University of North Carolina-Chapel Hill":3.6,"Johns Hopkins University":3.5,"New York University":3.4,"Pennsylvania State University-University Park":3.4,
-      "University of California-Irvine":3.4,"University of Minnesota-Twin Cities":3.4,"University of Virginia":3.4,"Northwestern University":3.3,"Ohio State University":3.3,"Rutgers , The State University of New Jersey-New Brunswick":3.3,"University of California-Davis":3.3,"University of California-Santa Barbara":3.3,"University of Chicago":3.3,"Dartmouth College":3.1,"Stony Brook University-SUNY":3.1,
-      "Texas A & M University-College Station":3.1,"University of Arizona":3.1,"University of Colarado-Boulder":3.1,"University of Utah":3.1,"Virginia Tech":3.1,"Washington University St. Louis":3.1,"Arizona State University":3.0}
+company_dict=[('Google ',5.0),('Microsoft ',5.0),('Tata Consultancy Services ',3.0),('Freshdesk ',4.0),('Tally ',4.0),('Good Methods Global ',3.9),('OLA ',4.1),('Freecharge ',4.2)]
+#########
 
-stop_words=set(stopwords.words('english'))
+#FRAMEWORKS
+frameworks=[("php","laravel"),("javascript","nodejs"),("python","django"),("python","data analytics"),("php","code igniter"),("php","zend framework"),("php","cake php")]
+##########
 
-words=word_tokenize(university)
-
-filtered_sentence=[]
-
-stop_words.remove("of")
-
-
-# Remove stop words
-
-for w in words:
-    if w not in stop_words:
-        filtered_sentence.append(w)
-
-list1=[]  
-     
-print(filtered_sentence)
-for word in filtered_sentence:
-    if word[0].isupper() or word=='of' or word=="," or word=="&":
-        list1.append(word)
-list1.remove("I")
-x=" ".join(list1)
-#print(x)
-print(exDict[x])
+#SKILLS
+skill_dict=["php","javascript","html","c#","asp.net","java","laravel","nodejs","django","python","ml","c","c++","mysql","mongodb","postgresql","ajax","ruby","perl","sqlite","vbscript"]
