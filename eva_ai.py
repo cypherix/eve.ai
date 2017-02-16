10ba5de0f3debf11f92f3309e65a88a8843b38ce
Rohit Anil <rohitanil7121993@gmail.com>
2017-02-16 17:47:13 +0530

    Implement Gaussian NB classifier

--- eva_ai.py
+++ eva_ai.py
@@ -7,6 +7,7 @@ import re
 import time
 import string
 import dictionary
+import GNB
 
 college=""
 company=""
@@ -15,6 +16,34 @@ college_score=0.0
 company_score=0.0
 conclusion2=""
 start=time.time()
+
+def entity_recog(text):
+        l=""
+        name=[]
+        final_list=[] #final list of colleges
+        # Change the path according to your system
+        stanford_classifier = '/Users/continuumlabs/Desktop/stanford/stanford-ner-2016-10-31/classifiers/english.all.3class.distsim.crf.ser.gz'
+        stanford_ner_path = '/Users/continuumlabs/Desktop/stanford/stanford-ner-2016-10-31/stanford-ner.jar'
+
+        # Creating Tagger Object
+        st = StanfordNERTagger(stanford_classifier, stanford_ner_path, encoding='utf-8')
+
+        tokenized_text = word_tokenize(text)
+        classified_text = st.tag(tokenized_text)
+        for sublist in classified_text:
+            if sublist[1]=='ORGANIZATION':
+                l+=str(sublist[0])
+                l+=" "
+            elif sublist[1]=='LOCATION':
+                l+=str(sublist[0])
+                        
+            elif sublist[0]=='.':
+                final_list.append(l)
+                l=""
+                
+                
+        final_list=list(filter(None,final_list))
+        return final_list
 
 
 #Location of file
@@ -52,7 +81,7 @@ with open('/Users/continuumlabs/Desktop/stanford/resume.txt', 'r') as f:
                 if(jd1[i]==skill_list[j]):
                     skill_match.append(skill_list[j])
         #print "\nMatching skill set:"
-        #print skill_match
+        print skill_match
 
         #score generation
         len_skill_match=len(skill_match)
@@ -76,82 +105,60 @@ with open('/Users/continuumlabs/Desktop/stanford/resume.txt', 'r') as f:
     #regular expression to find educational institution
     if re.search(r'(Academia|ACADEMIA|Education|EDUCATION)((:|\s|-|:-)*)((\r\n|\r|\n)*)((.*?|\r\n|\r|\n)*)([A-Z]{2,})', data):
         conclusion1 = re.search(r'(Academia|ACADEMIA|Education|EDUCATION)((:|\s|-|:-)*)((\r\n|\r|\n)*)((.*?|\r\n|\r|\n)*)([A-Z]{2,})', data).group(0)
+        college+=str(conclusion1)
+        c1 =college.split(' ',1)[1]
+        if c1[-1]!=".": 
+           c1+="."  #to determine the end of extracted paragraph(if there is no fullstop)
+        college_list=entity_recog(c1)
+        for i in range(0,len(college_list)):
+         for (x,y) in dictionary.college_dict:
+            if(x==college_list[i]):
+                college_score=float(y)+college_score
+        college_score=(4*college_score)/len(college_list)
+    else:
+        college_score=0
+
     #regular expression to find work experience
     if re.search(r'(Work Experience|WORK EXPERIENCE)((:|\s|-|:-)*)((\r\n|\r|\n)*)((.*?|\r\n|\r|\n)*)([A-z])(:|-|(\n)+]{1,})', data):
         conclusion2=re.search(r'(Work Experience|WORK EXPERIENCE)((:|\s|-|:-)*)((\r\n|\r|\n)*)((.*?|\r\n|\r|\n)*)([A-z])(:|-|(\n)+]{1,})', data).group(0)
-        
-    college+=str(conclusion1)
-    company+=str(conclusion2)
-    c1 =college.split(' ',1)[1]
-    if c1[-1]!=".":
-        c1+="."  #to determine the end of extracted paragraph(if there is no fullstop)
-
-    c2 =company.split(' ',1)[1]
-    if c2[-1]!=".":
-        c2+="."  #to determine the end of extracted paragraph(if there is no fullstop)
-
- 
-    f.seek(0,0) #point to beginning of file
-    firstNlines=f.readlines()[0:5]#read first 5 lines
-    firstNlines= list(map(lambda s: s.strip(), firstNlines))#strip newline characters 
-    data_sect2=re.findall(r'[\w\.-]+@[\w\.-]+',data)#email regex
-    email=str(data_sect2)  
-
-    def entity_recog(text):
-        l=""
-        final_list=[] #final list of colleges
-        # Change the path according to your system
-        stanford_classifier = '/Users/continuumlabs/Desktop/stanford/stanford-ner-2016-10-31/classifiers/english.all.3class.distsim.crf.ser.gz'
-        stanford_ner_path = '/Users/continuumlabs/Desktop/stanford/stanford-ner-2016-10-31/stanford-ner.jar'
-
-        # Creating Tagger Object
-        st = StanfordNERTagger(stanford_classifier, stanford_ner_path, encoding='utf-8')
-
-        tokenized_text = word_tokenize(text)
-        classified_text = st.tag(tokenized_text)
-        for sublist in classified_text:
-            if sublist[1]=='ORGANIZATION':
-                l+=str(sublist[0])
-                l+=" "
-            elif sublist[1]=='LOCATION':
-                l+=str(sublist[0])
-                        
-            elif sublist[0]=='.':
-                final_list.append(l)
-                l=""
-        final_list=list(filter(None,final_list))
-        return final_list
-
-    college_list=entity_recog(c1)
-    company_list=entity_recog(c2)
-    for i in range(0,len(college_list)):
-        for (x,y) in dictionary.college_dict:
-            if(x==college_list[i]):
-                college_score=float(y)+college_score
-
- 
-    for i in range(0,len(company_list)):
-        for (x,y) in dictionary.company_dict:
+        company+=str(conclusion2)
+        c2 =company.split(' ',1)[1]
+        if c2[-1]!=".":
+            c2+="."  #to determine the end of extracted paragraph(if there is no fullstop)
+        company_list=entity_recog(c2)
+        for i in range(0,len(company_list)):
+         for (x,y) in dictionary.company_dict:
             if(x==company_list[i]):
                 company_score=float(y)+company_score
-
-    company_score=(10*company_score)/len(company_list)
-    college_score=(4*college_score)/len(college_list)
+        company_score=(10*company_score)/len(company_list)
+    else:
+        company_score=0
+        
+  
+    f.seek(0,0) #point to beginning of file
+    #firstNlines=f.readlines()[0:2]#read first 2 lines
+    #firstNlines= list(map(lambda s: s.strip(), firstNlines))#strip newline characters
+    #fnl=" ".join(firstNlines)
+    #fnl+="."
+    #print(fnl)
+    data_sect2=re.findall(r'[\w\.-]+@[\w\.-]+',data)#email regex
+    email=str(data_sect2)  
     skill_score=20*skill_score
     total_score=company_score+college_score+skill_score
- 
+    if(email):
+        total_score=total_score+5
  
     
 #print "Extracted Details:"          
 #print college_score
-#print company_score
-#print (firstNlines)
-#print email
+print company_list
+#print len(email)
 #print "\nSCORE"
 #print skill_score#percent
-print "Total Score out of 90"
+print "Total Score out of 95"
 print total_score
-#Average runtime:11.65
+print(GNB.machine_learning(total_score))
+#Average runtime:6.068s
 print time.time()-start
 
 
